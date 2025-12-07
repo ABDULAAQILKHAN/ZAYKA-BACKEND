b093ef4601eca5c166d13bcec6074fd39fa4d221
Aaqil khan <aaqilpro99@gmail.com>
2025-12-08 01:49:54 +0530

    Made deployment ready: TEST 1

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,9 @@ async function bootstrap() {
   const { app } = await AppFactory.create();
   app.use(json({ limit: '10mb' }));
   app.use(urlencoded({ extended: true, limit: '10mb' }));
-  await app.listen(4000);
-  console.log(`🚀 Running at http://localhost:${4000}`);
+  const port = process.env.PORT || 4000;
+  await app.listen(port);
+  console.log(`🚀 Running at http://localhost:${port}`);
 }
 const vercel = false
 // Run bootstrap only if not on Vercel
